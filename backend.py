823226aa3a07f4b07d04f227bd5444bc9cd3e740
Saurabh Biswas <visnuk252@gmail.com>
2023-02-21 17:32:56 +0530

    Added Weather condition and icons

--- backend.py
+++ backend.py
@@ -3,21 +3,15 @@ import requests
 api_key = "3938d0f2387066d10e7199be37fdf4d5"
 
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={api_key}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data["list"]
     no_of_observations = 8 * forecast_days
     filtered_data = filtered_data[:no_of_observations]
-
-    if kind == "Temperature":
-        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
-    elif kind == "Weather Condition":
-        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
-
     return filtered_data
 
 
 if __name__ == "__main__":
-    get_data(place="Kolkata", forecast_days=2, kind="Weather Condition")
+    get_data(place="Kolkata", forecast_days=2)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+# Adding Title, Input, Slider, selectbox and subheader
 st.title("Weather forcast for Next Days")
 
 place = st.text_input("Place: ")
@@ -12,10 +13,21 @@ option = st.selectbox("Select the data to view", ("Temperature",
 
 st.subheader(f"{option} for the next {days} in {place}")
 
-get_data(place, days, option)
+if place:
+    # Getting the data for temperature/weather condition
+    filtered_data = get_data(place, days)
 
-dates = [days]
-temperature = [option]
-figure = px.line(x=dates, y=temperature, labels={"x:" "Dates",
-                                                 "y:" "Temperature(C)"})
-st.plotly_chart(figure)
+    if option == "Temperature":
+        # Creating a Temperature plot
+        temp = [dict["main"]["temp"] for dict in filtered_data]
+        dates = [dict["dt_txt"] for dict in filtered_data]
+        figure = px.line(x=dates, y=temp, labels={"x:" "Dates",
+                                                  "y:" "Temperature(C)"})
+        st.plotly_chart(figure)
+
+    elif option == "Weather Condition":
+        condition = [dict["weather"][0]["main"] for dict in filtered_data]
+        images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                  "Rain": "images/rain.png", "Snow": "images/snow.png"}
+        image_path = [images[sky] for sky in condition]
+        st.image(image_path, width=100)
